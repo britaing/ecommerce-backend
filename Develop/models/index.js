ba2e20641274e5e7b9210ead9e3987f6984ce1b3
Britain <britaing@gmail.com>
2023-09-13 22:41:28 -0500

    started index models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,11 +6,30 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 
+Products.belongsTo(Category);
+
 // Categories have many Products
+
+Categories.hasMany(Products);
 
 // Products belongToMany Tags (through ProductTag)
 
+Products.belongsToMany(Tags, () => {
+  through: {
+    model: ProductTag,
+    unique: false
+
+  },
+  as
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tags.belongsToMany(Products, () => {
+  through: {
+
+  }
+});
 
 module.exports = {
   Product,
